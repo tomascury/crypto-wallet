68be24ff8101e1e83bcac8755f94f739541a5b34
Tomas Cury <tomascury@gmail.com>
2024-11-23 11:52:55 +0000

    Entity mapping adjustment.

--- src/main/java/com/crypto/wallet/core/domain/Wallet.java
+++ src/main/java/com/crypto/wallet/core/domain/Wallet.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Wallet {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
   @Column(name = "name", nullable = false)
